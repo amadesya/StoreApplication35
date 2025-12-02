e8a199753bcaf2895688d46af77dc28ca1c9be18
0109-12 <0109-12@0109-12>
2025-12-02 13:56:40 +0300

    сортировка

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StoreApplication35.Models;
 
@@ -19,13 +20,20 @@ namespace StoreApplication35.Pages.Products
         public string Search {  get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string Sort {  get; set; }
+        public string Sort { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int Filter { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             if (HasRole() is IActionResult result)
                 return result;
-            
+
+            var manufacturers = _context.Manufacturers.ToList();
+            manufacturers.Insert(0, new Manufacturer { Id = 0, Name = "Все производители"}); 
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Name");
+
             Product = await _context.Products
                 .Include(p => p.Manufacturer)
                 .Include(p => p.Supplier).ToListAsync();
@@ -42,6 +50,10 @@ namespace StoreApplication35.Pages.Products
             if (Sort != null && Sort == "price_desc")
                 Product = Product
                     .OrderByDescending(p => p.Price)
+                    .ToList();
+            if (Filter != null && Filter > 0)
+                Product = Product
+                    .OrderByDescending(p => p.ManufacturerId == Filter)
                     .ToList();
             
             return Page();
